bd6cd4a5c93e9e5db4639f79f57ab944cb37e073
Dan <dannyrechtman@gmail.com>
2024-04-16 21:27:48 -0400

    changed redirect uri

--- src/routers/sso.py
+++ src/routers/sso.py
@@ -19,7 +19,7 @@ class SpotifyAuth(BaseModel):
     response_type:str = 'code'
     client_id:str = os.environ.get("CLIENT_ID")
     scope:str = "user-read-private user-read-email playlist-read-private playlist-read-collaborative"
-    redirect_uri:str = "http://127.0.0.1:80/sso/spotify/callback"
+    redirect_uri:str = "http://127.0.0.1:8000/sso/spotify/callback"
 
 
     class Config:
@@ -35,15 +35,21 @@ class SpotifyAuthResponse(BaseModel):
     refresh_token:str
 @router.get("/spotify")
 async def spotify(request:Request, model:SpotifyAuth = Depends()):
-    print(str(request.url))
-    model.redirect_uri = str(request.url).replace("http","https") + "/callback"
+    if bool(os.environ.get("PROD")) :
+        
+        model.redirect_uri = "https://semnity.com/sso/spotify/callback"
+    else:
+        print( str(request.url) + "/callback")
+        model.redirect_uri = str(request.url) + "/callback"
+
+
     
     return RedirectResponse(f"https://accounts.spotify.com/authorize?response_type={model.response_type}&client_id={model.client_id}&scope={model.scope}&redirect_uri={model.redirect_uri}")
 
 
 @router.get("/spotify/callback")
 async def spotify_callback(request:Request,model:SpotifyCallback = Depends()):
-
+    print(request)
     auth = base64.b64encode(f"{ os.environ.get('CLIENT_ID')}:{ os.environ.get('CLIENT_SECRET')}".encode("utf-8")).decode("ascii")
     value:Response = post(
         url="https://accounts.spotify.com/api/token",
@@ -60,7 +66,7 @@ async def spotify_callback(request:Request,model:SpotifyCallback = Depends()):
     dictionay = dict(value.json())
     models:SpotifyAuthResponse = SpotifyAuthResponse(**dictionay)
 
-    response = RedirectResponse("/#")
+    response = RedirectResponse("/")
     
     
     response.set_cookie("access_token",models.access_token)
